dce25380c103e52316b2e8ec71d112f50de9105d
hoarus <hoarus@gmail.com>
2022-01-05 20:18:33 +1100

    Add search functionality to flights index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,21 @@ class FlightsController < ApplicationController
     @departure_options = Flight.all.map{ |flight| [ flight.departure_airport.city ] }.uniq
     @arrival_options = Flight.all.map{ |flight| [ flight.arrival_airport.city ] }.uniq
     @date_options = Flight.all.map{ |flight| [ flight.start_datetime ] }.uniq
+
+    if(params.include?(:departure_airport))
+      @departure_city =  (Airport.find_by city: params[:departure_airport])
+      @flights = @flights.where(departure_airport: @departure_city )
+    end
+
+    if(params.include?(:arrival_airport))
+      @arrival_city =  (Airport.find_by city: params[:arrival_airport])
+      @flights = @flights.where(arrival_airport: @arrival_city )
+    end
+
+    if(params.include?(:date) && (params[:date] != nil))
+      @flights = @flights.where("start_datetime LIKE ?", params[:date] + "%")
+    end
+
+
   end
 end

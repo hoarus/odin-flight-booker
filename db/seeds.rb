3d4c6b004ed6cbd61caeede402d4d1963c99c6b1
hoarus <hoarus@gmail.com>
2022-10-25 17:47:27 -0600

    Update database seed for US

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,6 @@ Airport.delete_all
 
 
 Airport.create(city: "Denver", code: "DEN")
-Airport.create(city: "Jackson", code: "JAC")
-Airport.create(city: "Rapid City", code: "RAP")
 Airport.create(city: "Salt Lake City", code: "SLC")
 Airport.create(city: "Sioux Falls", code: "FSD")
 Airport.create(city: "Santa Fe", code: "SAF")
@@ -24,16 +22,25 @@ Airport.create(city: "Santa Fe", code: "SAF")
 # Flights
 
 Flight.delete_all
-  #Duplicate for dates (December Only)
-  date = 0
-  31.times do 
-    date +=1
-    #Duplicate for times 
-    6.times do |i|
-      Flight.create(
-        departure_airport: (Airport.find_by code: 'DEN'), arrival_airport: (Airport.find_by code: 'JAC'),
-        duration: '1:30', start_datetime: "2022-12-#{date} [0#{9 + i * 2}:30:00]"
-      )
+  airportCodes = ["DEN", "SLC", "FSD", "SAF"]
+  # Duplicate for all departing airports
+  for departingCode in airportCodes do
+    # Duplicate for all arriving airports
+    for arrivalCode in airportCodes do 
+      # Duplicate for dates (December Only)
+      date = 0
+      unless departingCode == arrivalCode 
+        25.times do 
+          date +=1
+          # Duplicate for times 
+          5.times do |i|
+            Flight.create(
+              departure_airport: (Airport.find_by code: departingCode), arrival_airport: (Airport.find_by code: arrivalCode),
+              duration: '1:30', start_datetime: "2022-12-#{date} [0#{9 + i * 2}:30:00]"
+            )
+          end
+        end
+      end
     end
   end
 

afbc3337eae4fbb1df912e9e3a8fd1929a42cb73
hoarus <hoarus@gmail.com>
2022-10-25 10:43:14 -0600

    Replace radio button with container-selection

--- app/javascript/packs/select_flight.js
+++ app/javascript/packs/select_flight.js
@@ -1,17 +1,30 @@
-console.log('easd')
-
-
-
-function selectFlight(elements, element) {
+function deselectAllFlights(elements) {
   for (let container of elements) {
     container.classList.remove('container-selected')
+    const radioContainer = container.querySelector(".flight-radio");
+    const radio = radioContainer.querySelector("input");
+    radio.checked = false;
   }
-  element.classList.add('container-selected')
 }
 
+function selectFlight(element) {
+  element.classList.add('container-selected')
+  const radioContainer = element.querySelector(".flight-radio");
+  const radio = radioContainer.querySelector("input");
+  radio.checked = true;
+}
+
+function updateFlightSelection(elements, element) {
+  deselectAllFlights(elements);
+  selectFlight(element);
+}
+
+// Query Selector
 const flight_containers = document.getElementsByClassName("flight-container");
 
+//Initialise function and event listeners
 for (const container of flight_containers) {
-  container.onclick = () => selectFlight(flight_containers, container)
+  deselectAllFlights(flight_containers);
+  container.onclick = () => updateFlightSelection(flight_containers, container)
 }
 

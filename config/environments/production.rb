1c4c1876966528330b2d50651ab3c2c90503654a
hoarus <hoarus@gmail.com>
2022-10-27 15:42:28 -0600

    Troubleshoot prod mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,9 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # Send email
+  # Configure email
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://freeflightbooker.herokuapp.com" }
+  
 end
